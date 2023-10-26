4ce0e365adc4a47f1bbf788aa53412689ef09b24
Aravind8331 <143053512+Aravind8331@users.noreply.github.com>
2023-10-25 21:33:35 -0500

    bonus

---
+++ routes/computation.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+let x=Math.floor(Math.random());
+let y
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    if(req.query.x!=undefined){
+        x=req.query.x;
+
+    }
+    let r=Math.cos(x);
+    let t=Math.asin(x);
+    let n=Math.asinhsinh(x);
+    
+  res.render('computation', { x:x,cos:r, asin:t, asinh:n});
+});
+
+module.exports = router;
